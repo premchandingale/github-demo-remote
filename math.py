213f3d41bc6e83af1746c4e5ecc6676eaadcaab4
premchand_ingale <premchand_ingale@persistent.com>
2022-01-09 01:31:28 +0530

    Syntax correction in math.py

--- math.py
+++ math.py
@@ -1,22 +1,22 @@
-# Add implementation
+# Add Implementation
 def add(x,y):
 	return x+y
 
-# Substract implementation
+# Substract Implementation
 def substract(x,y):
 	return x-y	#on master branch
 
-# Multiply implementation
+# Multiply Implementation
 def multiply(x,y):
 	return x*y	#on Bug456
 
-# Divide implementation
+# Divide Implementation
 def divide(x,y):
 	if y==0:
 		return DIVIDE_BY_0_ERROR
 	else:
 		return x/y
 
-# Square implementation	#before stash
+# Square Implementation	#before stash
 def square(x):
 	return x^2
